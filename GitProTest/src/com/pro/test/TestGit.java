c8cc39215c68362296bfbbd1ba64f2a454dfc29e
yang <yang@yang-pc>
2017-03-05 17:59:08 +0800

    kjhk

--- GitProTest/src/com/pro/test/TestGit.java
+++ GitProTest/src/com/pro/test/TestGit.java
@@ -8,6 +8,9 @@ public class TestGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		
+		
 		System.out.println("aa");
 		System.out.println("success to upload");
 		System.out.println("what happened");
