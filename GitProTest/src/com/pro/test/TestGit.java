433f006c8a6e4ad97d139195cba9cae6d913fba6
yang <yang@yang-pc>
2017-03-05 17:46:12 +0800

    FirstTest

---
+++ GitProTest/src/com/pro/test/TestGit.java
@@ -0,0 +1,14 @@
+package com.pro.test;
+
+public class TestGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("aa");
+	}
+
+}
